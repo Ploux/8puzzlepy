4afdb8630744c4594b1e2917fb48727f2db91d02
Ploux <43018474+Ploux@users.noreply.github.com>
2022-04-23 14:05:46 -0500

    add initial file

---
+++ algos.py
@@ -0,0 +1,235 @@
+"""
+algos.py
+4/23/22
+1. Determine which algorithms are possible to implement
+2. Determine the data structures needed
+3. Prepare to convert to javascript
+"""
+
+import random
+import heapq
+import math
+import sys
+
+from collections import defaultdict, deque, Counter
+from itertools import combinations
+
+## PROBLEM
+
+class Problem(object):
+
+    """
+    The abstract class for a formal problem.
+    A new domain subclasses this, overriding `actions` and `results`, and perhaps other methods.
+    The default heuristic is 0 and the default action cost is 1 for all states.
+    When you create an instance of a subclass, specify `initial`, and `goal` states  (or give an `is_goal` method) and perhaps other keyword args for the subclass.
+
+    Q: do we need this (i.e. can we make a more specific one just for 8 puzzle), and how is it used
+    """
+    def __init__(self, initial=None, goal=None, **kwds):
+        self.__dict__.update(initial=initial, goal=goal, **kwds)
+
+    def actions(self, state):        raise NotImplementedError
+    def result(self, state, action): raise NotImplementedError
+    def is_goal(self, state):        return state == self.goal
+    def action_cost(self, s, a, s1): return 1
+    def h(self, node):               return 0
+
+    def __str__(self):
+        return '{}({!r}, {!r})'.format(
+            type(self).__name__, self.initial, self.goal)
+
+
+## NODE
+
+class Node:
+    "A Node in a search tree."
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.__dict__.update(state=state, parent=parent, action=action, path_cost=path_cost)
+
+    def __repr__(self): return '<{}>'.format(self.state)
+    def __len__(self): return 0 if self.parent is None else (1 + len(self.parent))
+    def __lt__(self, other): return self.path_cost < other.path_cost
+
+
+failure = Node('failure', path_cost=math.inf) # Indicates an algorithm couldn't find a solution.
+cutoff  = Node('cutoff',  path_cost=math.inf) # Indicates iterative deepening search was cut off.
+
+
+def expand(problem, node):
+    "Expand a node, generating the children nodes."
+    s = node.state
+    for action in problem.actions(s):
+        s1 = problem.result(s, action)
+        cost = node.path_cost + problem.action_cost(s, action, s1)
+        yield Node(s1, node, action, cost)
+
+
+def path_actions(node):
+    "The sequence of actions to get to this node."
+    if node.parent is None:
+        return []
+    return path_actions(node.parent) + [node.action]
+
+
+def path_states(node):
+    "The sequence of states to get to this node."
+    if node in (cutoff, failure, None):
+        return []
+    return path_states(node.parent) + [node.state]
+
+
+## QUEUE
+
+"""
+First-in-first-out and Last-in-first-out queues, and a PriorityQueue, which allows you to keep a collection of items, and continually remove from it the item with minimum f(item) score.
+
+We will need to implement FIFOQueue and LIFOQueue in JS
+"""
+
+FIFOQueue = deque
+
+LIFOQueue = list
+
+class PriorityQueue:
+    # A queue in which the item with minimum f(item) is always popped first.
+
+    def __init__(self, items=(), key=lambda x: x):
+        self.key = key
+        self.items = [] # a heap of (score, item) pairs
+        for item in items:
+            self.add(item)
+
+    def add(self, item):
+        # Add item to the queue.
+        pair = (self.key(item), item)
+        heapq.heappush(self.items, pair)
+
+    def pop(self):
+        # Pop and return the item with min f(item) value.
+        return heapq.heappop(self.items)[1]
+
+    def top(self): return self.items[0][1]
+
+    def __len__(self): return len(self.items)
+
+## Search Algorithms: Best-First
+
+"""Best-first search with various f(n) functions gives us different search algorithms. Note that A*, weighted A* and greedy search can be given a heuristic function, h, but if h is not supplied they use the problem's default h function (if the problem does not define one, it is taken as h(n) = 0).
+
+start with best_first_search => breadth_first_bfs
+1. remove all other algos until minimum code remains
+2. fix tile order
+3. try measurement algorithm
+"""
+
+def best_first_search(problem, f):
+    "Search nodes with minimum f(node) value first."
+    node = Node(problem.initial)
+    frontier = PriorityQueue([node], key=f)
+    reached = {problem.initial: node}
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            return node
+        for child in expand(problem, node):
+            s = child.state
+            if s not in reached or child.path_cost < reached[s].path_cost:
+                reached[s] = child
+                frontier.add(child)
+    return failure
+
+def breadth_first_bfs(problem):
+    "Search shallowest nodes in the search tree first; using best-first."
+    return best_first_search(problem, f=len)
+
+
+### 8 Puzzle Problems
+
+"""A sliding tile puzzle where you can swap the blank with an adjacent piece, trying to reach a goal configuration. The cells are numbered 0 to 8, starting at the top left and going row by row left to right. The pieces are numebred 1 to 8, with 0 representing the blank. An action is the cell index number that is to be swapped with the blank (not the actual number to be swapped but the index into the state). So the diagram above left is the state (5, 2, 7, 8, 4, 0, 1, 3, 6), and the action is 8, because the cell number 8 (the 9th or last cell, the 6 in the bottom right) is swapped with the blank.
+
+There are two disjoint sets of states that cannot be reached from each other. One set has an even number of "inversions"; the other has an odd number. An inversion is when a piece in the state is larger than a piece that follows it."""
+
+
+class EightPuzzle(Problem):
+    """ The problem of sliding tiles numbered from 1 to 8 on a 3x3 board,
+    where one of the squares is a blank, trying to reach a goal configuration.
+    A board state is represented as a tuple of length 9, where the element at index i
+    represents the tile number at index i, or 0 if for the empty square, e.g. the goal:
+        1 2 3
+        4 5 6 ==> (1, 2, 3, 4, 5, 6, 7, 8, 0)
+        7 8 _
+    """
+
+    def __init__(self, initial, goal=(1, 2, 3, 4, 5, 6, 7, 8, 0)):
+        assert inversions(initial) % 2 == inversions(goal) % 2 # Parity check
+        self.initial, self.goal = initial, goal
+
+    def actions(self, state):
+        """The indexes of the squares that the blank can move to."""
+        moves = ((1, 3),    (0, 2, 4),    (1, 5),
+                 (0, 4, 6), (1, 3, 5, 7), (2, 4, 8),
+                 (3, 7),    (4, 6, 8),    (7, 5))
+        blank = state.index(0)
+        return moves[blank]
+
+    def result(self, state, action):
+        """Swap the blank with the square numbered `action`."""
+        s = list(state)
+        blank = state.index(0)
+        s[action], s[blank] = s[blank], s[action]
+        return tuple(s)
+
+    def h1(self, node):
+        """The misplaced tiles heuristic."""
+        return hamming_distance(node.state, self.goal)
+
+    def h2(self, node):
+        """The Manhattan heuristic."""
+        X = (0, 1, 2, 0, 1, 2, 0, 1, 2)
+        Y = (0, 0, 0, 1, 1, 1, 2, 2, 2)
+        return sum(abs(X[s] - X[g]) + abs(Y[s] - Y[g])
+                   for (s, g) in zip(node.state, self.goal) if s != 0)
+
+def inversions(board):
+    "The number of times a piece is a smaller number than a following piece."
+    return sum((a > b and a != 0 and b != 0) for (a, b) in combinations(board, 2))
+
+
+def board8(board, fmt=(3 * '{} {} {}\n')):
+    "A string representing an 8-puzzle board"
+    return fmt.format(*board).replace('0', '_')
+
+class Board(defaultdict):
+    empty = '.'
+    off = '#'
+    def __init__(self, board=None, width=8, height=8, to_move=None, **kwds):
+        if board is not None:
+            self.update(board)
+            self.width, self.height = (board.width, board.height)
+        else:
+            self.width, self.height = (width, height)
+        self.to_move = to_move
+
+    def __missing__(self, key):
+        x, y = key
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            return self.off
+        else:
+            return self.empty
+
+    def __repr__(self):
+        def row(y): return ' '.join(self[x, y] for x in range(self.width))
+        return '\n'.join(row(y) for y in range(self.height))
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.items()))) + hash(self.to_move)
+
+
+e5 = EightPuzzle((8, 6, 7, 2, 5, 4, 3, 0, 1))
+## e5p = EightPuzzle((1, 6, 7, 2, 5, 4, 3, 8, 0)) will get parity error
+
+for s in path_states(breadth_first_bfs(e5)):
+    print(board8(s))
+
+
